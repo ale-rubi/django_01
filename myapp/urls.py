63f6188f7086a4f985c81a3dd922fd9de598ec45
Ale Torrez <ar.torrez@acad.ucb.edu.bo>
2024-05-11 21:01:55 -0400

    Style and models update

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('about/', views.about,name="about"),
     path('project/', views.project,name="project"),
+    path('create_project/',views.create_project,name="create_project"),
+    path('project/<int:id>',views.project_detail,name="project_detail"),
     path('task/', views.task,name="task"),
     path('create_task/', views.create_task,name="create_task"),
-    path('create_project/',views.create_project,name="create_project"),
+    
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -49,4 +49,11 @@ def create_project(request):
     else:
         Project.objects.create(name=request.POST["name"])
         return redirect('project')
- 
+    
+def project_detail(request, id):
+    project =get_object_or_404(Project, id=id)
+    tasks = Task.objects.filter(project_id=id)
+    return render(request, 'projects/project_detail.html', {
+        'project':project,
+        'tasks':tasks,
+    })
